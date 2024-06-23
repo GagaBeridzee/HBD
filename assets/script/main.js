d9b3bc6271b5e74fec16d745a31e48a6391c7509
GagaBeridzee <gagaberidzee@gmail.com>
2024-06-23 22:36:02 +0400

    j

---
+++ assets/script/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".steps", {
+  navigation: {
+    nextEl: ".btn-primary",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+});
